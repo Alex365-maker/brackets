ff5b69b6f50616de702164381218f3d50373cfc3
Alex365-maker <70705449+Alex365-maker@users.noreply.github.com>
2021-08-25 15:07:36 +0300

    feat:
    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('')
+  let h = 0;
+  for(let i = 1; i < str.length; i++){
+    h = 0;
+    for(h = 0; h < bracketsConfig.length; h++){
+    if(str[i-1] == bracketsConfig[h][0] && str[i] == bracketsConfig[h][1]){
+    str.splice(i-1, 2);
+    i = 0;
+    }
+  }
+    }
+    if(str[0]==null){
+    return true;
+    }
+    else{return false}
 }
